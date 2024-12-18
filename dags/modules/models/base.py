edaf536822b5bce79e850b941a0ceb35b2d0b563
Vyacheslav Kostrov <slavkostrov@gmail.com>
2024-12-18 23:04:18 +0300

    [ADD] models

--- dags/modules/models/base.py
+++ dags/modules/models/base.py
@@ -1,16 +1,19 @@
 import logging
 import os
 
+from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base, declared_attr
 
 TABLE_PREFIX_ENV_VAR = "TABLE_PREFIX"
-TABLE_PREFIX = os.get(TABLE_PREFIX_ENV_VAR)
+TABLE_PREFIX = os.environ.get(TABLE_PREFIX_ENV_VAR)
 if not TABLE_PREFIX:
     raise RuntimeError("env variable 'TABLE_PREFIX_ENV_VAR' not found")
 else:
     logging.debug("found env variable for table_prefix: %s", TABLE_PREFIX)
 
-DeclarativeBase = declarative_base()
+
+metadata = MetaData(schema="public")
+DeclarativeBase = declarative_base(metadata=metadata)
 
 
 class Base:
